a35ef766ec8ad65f034c83cb5fd99dcc21f8998f
Sage Welky <ehrenwelky@gmail.com>
2024-10-22 14:50:55 -0500

    Created basic API call, API data handler, primeary scaffold html, basic formatting css, and very minimal styling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import "./styles.css";
+import { callWeatherAPI } from "./modules/apiUtils";
+import { displayUpdate } from "./modules/weatherDisplay";
+
+async function getWeatherData() {
+  let location = document.querySelector('.location-input').value;
+  let data = await callWeatherAPI(location);
+  //change to ouput information to display
+  console.log(data);
+  displayUpdate(data);
+}
+
+let searchButton = document.querySelector('.search-button');
+searchButton.addEventListener( 'click', getWeatherData );
+
+let locationInput = document.querySelector('.location-input');
+locationInput.addEventListener('keydown', function (e) {
+  if (e.which === 13 && !e.shiftKey) {
+    getWeatherData();
+  }
+});
+
--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: {
@@ -15,6 +16,7 @@ module.exports = {
       title: "Production",
       template: "./src/template.html",
     }),
+    new Dotenv(),
   ],
   module: {
     rules: [
